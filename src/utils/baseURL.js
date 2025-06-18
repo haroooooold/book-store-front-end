2806d2735cca9597e6f6e70635b202130a10c81c
Harold <haroldcuenca01@gmail.com>
2025-06-18 16:05:39 +0800

    redirect to publish back-end

--- src/utils/baseURL.js
+++ src/utils/baseURL.js
@@ -1,5 +1,6 @@
 const getBaseUrl = () => {
-  return "http://localhost:5000";
+  //return "http://localhost:5000";
+  return "https://book-store-back-end-beta.vercel.app";
 };
 
 export default getBaseUrl;
